ef129f5caf7c497022418c5dfd93516e468c67a8
bomzeric <bomz.eric@gmail.com>
2025-06-04 08:52:13 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,44 @@
+// Store a reference to the <h1> in a variable
+//const myHeading = document.querySelector("h1");
+// Update the text content of the <h1>
+//myHeading.textContent = "Hello world!";
+
+// Image switcher code
+
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+});
+
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
